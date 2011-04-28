eb6a74bc143fe9f23e36b517a6f99ac533868af5
rogerdpack <rogerpack2005@gmail.com>
2011-04-28 07:29:29 -0600

    init rails 3 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RelationshipController::Application.config.secret_token = 'd230a02b22bafa861c669c6843f5cd5c207094f76972d5821f053a59f4ac12cf8bec4990945c8aaf25c0b82e405fc84d41633fb6fc12d619424801587bb4d1b0'
