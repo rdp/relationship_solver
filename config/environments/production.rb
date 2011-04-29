de452bef34d8f9b933fc8a67c78e91831e2a0c18
rogerdpack <rogerpack2005@gmail.com>
2011-04-29 07:49:49 -0600

    try work in relative route. wow

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 RelationshipController::Application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # The production environment is meant for finished, "live" apps.
